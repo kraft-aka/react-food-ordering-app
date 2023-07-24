c0a14713a21fb4fa8e44ecdf7dc5f55b0be6dc38
kubat <kubat.aiupov@gmail.com>
2023-07-24 22:23:35 +0200

    add Meals comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Meals from "./components/Meal/Meals";
 import Header from "./components/Layout/Header";
 import "./styles.css";
 
 export default function App() {
-
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ src/components/Meal/Meals.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MealsSSummary from "./MealsSSummary";
+import MealsAvailable from "./MealsAvailable";
+
+export default function Meals() {
+  return (
+    <>
+      <MealsSSummary />
+      <MealsAvailable />
+    </>
+  );
+}
